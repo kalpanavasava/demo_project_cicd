162fd301cbe61319760f867852668ef831955e9e
kalpanavasava <105626130+kalpanavasava@users.noreply.github.com>
2024-12-27 15:46:52 +0530

    Update Test case

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,8 +6,12 @@ class ExampleTest extends TestCase
 {
     public function testAddition()
     {
-        $result = 2 + 2;
-        $this->assertEquals(4, $result);
+        ob_start();
+        include 'index.php';
+        $output = ob_get_clean();
+        
+        $this->assertStringContainsString("<h1>Welcome to GitHub Actions CI/CD Demo VW</h1>", $output);
+        $this->assertStringContainsString("Last deployment:", $output);
     }
 }
 
