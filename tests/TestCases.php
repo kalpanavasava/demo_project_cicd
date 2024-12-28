d6bb4688ed84946340262ccb9037833dd6fd47ec
kalpanavasava <105626130+kalpanavasava@users.noreply.github.com>
2024-12-28 17:06:03 +0530

    Update TestCases.php

--- tests/TestCases.php
+++ tests/TestCases.php
@@ -6,12 +6,8 @@ class TestCases extends TestCase
 {
     public function testIndex()
     {
-        ob_start();
-        include 'index.php';
-        $output = ob_get_clean();
-        
-        $this->assertStringContainsString("<h1>Welcome to GitHub Actions CI/CD Demo VW</h1>", $output);
-        $this->assertStringContainsString("Last deployment:", $output);
+        $response = $this->get('/app/index.php');
+        $response->assertSee('<h1>Welcome to GitHub Actions CI/CD Demo VW</h1>');
     }
 }
 
