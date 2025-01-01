1641a8ba0353fa1080542f9d73782edde55226f4
kalpanavasava <105626130+kalpanavasava@users.noreply.github.com>
2025-01-01 11:43:46 +0530

    Write test case for index.php

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -2,16 +2,15 @@
 
 use PHPUnit\Framework\TestCase;
 
-class TestCases extends TestCase
+class IndexTest extends TestCase
 {
     public function testIndex()
     {
         ob_start();
-        include 'app/index.php';
+        include 'public/index.php';
         $output = ob_get_clean();
         
         $this->assertStringContainsString("<h1>Welcome to GitHub Actions CI/CD Demo VW</h1>", $output);
-        $this->assertStringContainsString("Last deployment:", $output);
     }
 }
 
