d4dedf307f52ed8ecd4b62886d412ecce7ee99ba
kalpanavasava <105626130+kalpanavasava@users.noreply.github.com>
2025-01-15 12:30:49 +0530

    code update

--- custom-script/generate_coverage_report.php
+++ custom-script/generate_coverage_report.php
@@ -223,8 +223,6 @@ $html .= '}</script>';
 
 $html .= '</div></body></html>';
 
-echo $html;die;
-
 // Save the HTML report as a file and trigger download
 $reportFilePath = 'artifacts-coverage-report/coverage-report.html';
 
