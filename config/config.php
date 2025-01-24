c29978105addb7bcb8fd3d63875a3dae0a96c4f9
kalpanavasava <105626130+kalpanavasava@users.noreply.github.com>
2025-01-24 15:20:34 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php 
 /*...........Database Constants.................*/
-define ("DB_HOST","184.168.102.106");
-define ("DB_USER","demo_project_cicd");
-define ("DB_PASSWORD","C]3*]9I23sFa");
-define ("DB_NAME","demo_project_cicd");
+define ("CON_DB_HOST","184.168.102.106");
+define ("CON_DB_USER","demo_project_cicd");
+define ("CON_DB_PASSWORD","C]3*]9I23sFa");
+define ("CON_DB_NAME","demo_project_cicd");
