dc0dbf73aca36f8c5be5ba79cbcfc586cdcd299b
kalpanavasava <105626130+kalpanavasava@users.noreply.github.com>
2025-01-24 12:30:29 +0530

    Update db_connection.php

--- config/db_connection.php
+++ config/db_connection.php
@@ -1,8 +1,8 @@
-<?php
-$servername = "localhost"; 
-$username = "root";                           
-$password = "";                  
-$dbname = "demo_project_cicd";  
+<?php 
+$servername = "184.168.102.106";
+$username = "demo_project_cicd";
+$password = "C]3*]9I23sFa";
+$dbname = "demo_project_cicd";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if( !$conn ){
     die('Connection failed : ' . mysqli_connect_error());
