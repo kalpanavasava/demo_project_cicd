db991e3bd96dbad13b906acaa4f8ebf5029df13a
kalpanavasava <105626130+kalpanavasava@users.noreply.github.com>
2025-01-24 15:12:04 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php 
 /*...........Database Constants.................*/
-define ("DB_HOST","voidek.in");
+define ("DB_HOST","184.168.102.106");
 define ("DB_USER","demo_project_cicd");
 define ("DB_PASSWORD","C]3*]9I23sFa");
 define ("DB_NAME","demo_project_cicd");
